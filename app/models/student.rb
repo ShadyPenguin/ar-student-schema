954229bd9722e71f9577dd092955deb2e0f7402d
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-11 16:57:12 -0500

    Passed all tests for student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,13 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+  validates :email, :uniqueness => true
+  validates :age, :numericality => true 
+  validates :age, :numericality => { :greater_than => 5} 
+  validates :email, :format => { :with => /\S+@\S+\.\S{2,}/, 
+      :message => "Nah nah nah nah nah"}
+  validates :phone, :format => { :with => /\(\d{3}\)\s\d{3}-\d{4}\sx\d{4}/, 
+      :message => "That's not a phone number, you fool!"}
 
   def name 
     first_name + " " + last_name
