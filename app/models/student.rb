edb0c28b91e44d5c90042c9d0804b40a85b27ae6
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-11 15:58:13 -0500

    First Object-Relational Model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  def name 
+    first_name + " " + last_name
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - birthday.year
+  end
 end
